bdaf66fd7666b10c1898cb656ad06270f14580fc
Remi Garcia Herrera <rem.garcia@duocuc.cl>
2026-06-16 22:46:29 -0400

    feat: setup inicial frontend React + Vite + Tailwind + estructura base adicional se crean las primeras interfaces en ts para la recepcionde los datos desde el backend

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-navy flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-teal">
+        Donaton ✅ Tailwind funcionando
+      </h1>
+    </div>
+  )
+}
